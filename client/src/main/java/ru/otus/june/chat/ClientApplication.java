72c8e3f86893bcc41f84d22c3df7619149cdc6ad
TatyanaM <nusha_kup@mail.ru>
2024-06-27 06:53:09 +0300

    Initial commit

---
+++ client/src/main/java/ru/otus/june/chat/ClientApplication.java
@@ -0,0 +1,10 @@
+package ru.otus.june.chat;
+
+
+import java.io.IOException;
+
+public class ClientApplication {
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
+}
--- server/src/main/java/ru/otus/june/chat/server/ServerApplication.java
+++ server/src/main/java/ru/otus/june/chat/server/ServerApplication.java
@@ -1,25 +1,9 @@
 package ru.otus.june.chat.server;
 
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class ServerApplication {
     public static void main(String[] args) {
-        int port = 8189;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
-            Socket socket = serverSocket.accept();
-            System.out.println("Подключился новый клиент");
+        new Server(8189).start();
 
-            byte[] buffer = new byte[8192];
-            while (true) {
-                int n = socket.getInputStream().read(buffer);
-                String message = new String(buffer, 0, n);
-                System.out.print(message);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
